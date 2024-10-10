9f3901f1a2784cf7dd7797d628c1b6b9705cf56b
bngams <n.boris@devidia.net>
2024-10-10 13:01:34 +0000

    :white_check_mark: apply DRY & KISS

--- 2-intro-reactive-prog/index.mjs
+++ 2-intro-reactive-prog/index.mjs
@@ -9,26 +9,26 @@ const numberStream = from(numbers).pipe(
   concatMap(num => of(num).pipe(delay(500)))  // Simule l'émission d'un nombre toutes les 500ms
 );
 
-// Fonction réactive qui calcule la somme des nombres pairs au carré
-const sumOfSquaresOfEvens$ = numberStream.pipe(
-  filter(num => num % 2 === 0),  // Filtre (filter) les nombres pairs
-  map(num => num * num),  // Transformer (map) pour appliquer le nombre au carré
-  reduce((sum, num) => sum += num, 0)  // Faire la somme des résultats (reduce)
-);
+// Fonctions utilitaires
+const isEven = (num) => num % 2 === 0;
+const isOdd = (num) => !isEven(num);  // Ou num % 2 !== 0
+const square = (num) => num * num;
+const cube = (num) => num * num * num;
 
-// Fonction réactive qui calcule la somme des nombres impairs au cube
-const sumOfCubesOfOdds$ = numberStream.pipe(
-  filter(num => num % 2 !== 0),  // Filtre (filter) les nombres pairs
-  map(num => num * num * num),  // Transformer (map) pour appliquer le nombre au carré
-  reduce((sum, num) => sum += num, 0)  // Faire la somme des résultats (reduce)
-);
+// Fonction pour calculer la somme des transformations (carré ou cube) sur un flux filtré
+const calculateSum = (filterCondition, transformation) =>
+  numberStream.pipe(
+    filter(filterCondition),         // Filtrer les nombres
+    map(transformation),             // Appliquer la transformation (carré ou cube)
+    reduce((sum, num) => sum + num, 0)  // Faire la somme des résultats
+  );
 
 // Souscription pour afficher la somme des carrés des pairs
-sumOfSquaresOfEvens$.subscribe(sum => {
+calculateSum(isEven, square).subscribe(sum => {
   console.log("Somme des carrés des pairs:", sum);  // Affiche 220
 });
 
 // Souscription pour afficher la somme des cubes des impairs
-sumOfCubesOfOdds$.subscribe(sum => {
+calculateSum(isOdd, cube).subscribe(sum => {
   console.log("Somme des cubes des impairs:", sum);  // Affiche 1225
 });
