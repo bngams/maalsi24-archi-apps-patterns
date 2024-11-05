87c5d1f641daa06fa75cefb71ecb40568e06f65b
bngams <n.boris@devidia.net>
2024-11-05 13:25:06 +0000

    first working implementation

--- 4-nestjs-microservices/apps/auth-service/src/auth-service.controller.ts
+++ 4-nestjs-microservices/apps/auth-service/src/auth-service.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthServiceService } from './auth-service.service';
+import { Credentials } from './models/credentials';
 
 @Controller()
 export class AuthServiceController {
-  constructor(private readonly authServiceService: AuthServiceService) {}
+  constructor(private readonly authService: AuthServiceService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authServiceService.getHello();
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() credentials: Credentials) {
+    return this.authService.signIn(credentials.username, credentials.password);
   }
 }
