2c0f5741e301c22dd60d97d9ecba5e00636b2fc5
bngams <n.boris@devidia.net>
2024-11-05 13:02:15 +0000

    add users module and service

--- 4-nestjs-microservices/apps/auth-service/src/auth-service.module.ts
+++ 4-nestjs-microservices/apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
 import { AuthServiceService } from './auth-service.service';
+import { MongoModule } from './mongo.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongoModule,
+    UsersModule
+  ],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
